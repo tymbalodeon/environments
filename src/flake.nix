8b8ebc6d4fc5fc852169829c2eb530fbe68a84db
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-05-26 14:24:52 -0400

    feat: rust

--- src/flake.nix
+++ src/flake.nix
@@ -1,24 +1,64 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-  outputs = {nixpkgs, ...}: {
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: {
     devShells =
       nixpkgs.lib.genAttrs [
         "x86_64-darwin"
         "x86_64-linux"
       ] (
-        system: let
-          pkgs = import nixpkgs {
-            config.allowUnfree = true;
-            inherit system;
-          };
-        in
+        system:
           nixpkgs.lib.genAttrs
-          (builtins.attrNames (builtins.readDir ./.))
+          (builtins.attrNames (
+            builtins.listToAttrs (
+              builtins.filter
+              (item: item.value == "directory")
+              (nixpkgs.lib.attrsToList (builtins.readDir ./.))
+            )
+          ))
           (
-            environment:
+            environment: let
+              pkgs = import nixpkgs {
+                config.allowUnfree = true;
+
+                overlays =
+                  if environment == "rust"
+                  then [rust-overlay.overlays.default]
+                  else [];
+
+                inherit system;
+              };
+            in
               pkgs.mkShellNoCC (
-                import ./${environment}/flake.nix {inherit pkgs;}
+                let
+                  shell = import ./${environment}/shell.nix {inherit pkgs;};
+
+                  toolchain =
+                    pkgs.rust-bin.fromRustupToolchainFile
+                    ./rust/toolchain.toml;
+                in (
+                  if environment == "rust"
+                  then
+                    shell
+                    // {
+                      packages = with pkgs; [
+                        rust-analyzer-unwrapped
+                        toolchain
+                      ];
+
+                      shellHook = ''
+                        export RUST_SRC_PATH=${toolchain}/lib/rustlib/src/rust/library
+                      '';
+                    }
+                  else shell
+                )
               )
           )
       );
