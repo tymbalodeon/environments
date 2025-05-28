768df80343d5ca804a7352060cc422b8d3e969f3
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-05-28 18:48:22 -0400

    fix: python

--- src/python/shell.nix
+++ src/python/shell.nix
@@ -1,9 +1,15 @@
 {pkgs}: {
   packages = with pkgs; [
     nodePackages.pnpm
-    python313Packages.pipx
     python313
+    python313Packages.pipx
+    python313Packages.vulture
     ruff
     uv
   ];
+
+  shellHook = ''
+    uv sync
+    source .venv/bin/activate
+  '';
 }
