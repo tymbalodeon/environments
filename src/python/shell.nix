cc8425ecd310aa6696ca53d0bfa6e7dad51471c1
Ben Rosen <benrosen@upenn.edu>
2025-06-05 10:00:16 -0400

    fix: python

--- src/python/shell.nix
+++ src/python/shell.nix
@@ -4,7 +4,7 @@
     python313
     python313Packages.jedi-language-server
     python313Packages.pipx
-    python313Packages.python-python-lsp-server
+    python313Packages.python-lsp-server
     python313Packages.vulture
     ruff
     uv
