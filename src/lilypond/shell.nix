ed2a00954322662804898d56e25d4ea82d61966a
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-07-20 22:55:14 -0400

    fix: lilypond shell

--- src/lilypond/shell.nix
+++ src/lilypond/shell.nix
@@ -1,19 +1,10 @@
 {pkgs}: {
   packages = with pkgs; [
-    deadnix
-    fd
-    flake-checker
-    gh
     gyre-fonts
-    just
     lilypond-unstable-with-fonts
-    lychee
     nushell
     pre-commit
     python312Packages.pre-commit-hooks
-    ripgrep
-    statix
-    tokei
     watchexec
     zathura
     zellij
