df995d0678723089ee5fad8e4a5dfb81b9244db1
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-07-21 20:46:21 -0400

    fix: ruby gem_path

--- src/ruby/shell.nix
+++ src/ruby/shell.nix
@@ -3,5 +3,18 @@
     ruby
   ];
 
-  shellHook = "export PATH=$PATH:$HOME/.local/share/gem/bin";
+  shellHook = "
+    gem_bin_paths=$(
+      nu -c '
+        ls ~/.local/share/gem/ruby
+        | get name
+        | each {ls $in | where name =~ /bin$}
+        | flatten
+        | get name
+        | str join :
+      '
+    )
+
+    export PATH=$PATH:$gem_bin_paths
+  ";
 }
