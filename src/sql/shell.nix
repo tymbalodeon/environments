85cb6beb96cb843948ac1159174ddb42c8d2e7d0
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-08-08 15:26:55 -0400

    feat: init sql

---
+++ src/sql/shell.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  packages = with pkgs; [
+    # TODO: should postgres be a feature/option of sql, with other options like mysql?
+    pgformatter
+    postgres-lsp
+    postgresql
+    # TODO: make this a feature?
+    postgrest
+    # TODO: should this be the default over pgformatter, with the latter as a feature?
+    sqlfluff
+  ];
+}
