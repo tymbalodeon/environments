25646e94500888c9877073af1e360a9865c0072f
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-11-04 13:15:03 -0500

    fix: postgres-language-server

--- src/sql/shell.nix
+++ src/sql/shell.nix
@@ -2,7 +2,7 @@
   packages = with pkgs; [
     # TODO: should postgres be a feature/option of sql, with other options like mysql?
     pgformatter
-    postgres-lsp
+    postgres-postgres-language-server
     postgresql
     # TODO: make this a feature?
     postgrest
