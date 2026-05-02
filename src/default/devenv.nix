4a1551e6ecfde6ede4155fe04a30791962874896
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-05-01 21:06:15 -0400

    chore: try importing default environments

--- src/default/devenv.nix
+++ src/default/devenv.nix
@@ -4,14 +4,11 @@
   pkgs,
   ...
 }: {
-  # TODO: can this pull in everything from the other module folders without
-  # having to add them to the devenv.yaml file? this may require restructring so
-  # this is in a higher level folder than the others?
   files = import "${inputs.environments}/files.nix" {
     inherit lib;
 
     environment = "default";
-    files = ./files;
+    files = ./default/files;
   };
 
   packages = with pkgs; [
@@ -26,6 +23,7 @@
     ripgrep
     # TODO: this is for css; move elsewhere
     stylelint
+    taplo
     # TODO: what are these used for?
     vscode-langservers-extracted
   ];
