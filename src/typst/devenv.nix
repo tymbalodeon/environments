b0ff37564265c497dd457db13f7fd88d2421fbed
Ben Rosen <benjamin.j.rosen@gmail.com>
2026-05-23 23:07:51 -0400

    fix: typst

--- src/typst/devenv.nix
+++ src/typst/devenv.nix
@@ -1,16 +1,5 @@
-{
-  inputs,
-  lib,
-  pkgs,
-  ...
-}: {
-  files = import "${inputs.environments}/files.nix" {
-    inherit lib;
-
-    environment = "typst";
-    files = ./files;
-  };
-
+{pkgs, ...}: {
+  env.ENVIRONMENTS_TYPST_ZELLIJ_LAYOUT = ./layout.kdl;
   languages.typst.enable = true;
 
   packages = with pkgs; [
--- src/typst/scripts/watch.nu
+++ src/typst/scripts/watch.nu
@@ -9,7 +9,7 @@ def main [file?: string] {
 
   let layout_file = (mktemp --tmpdir $"($file)-XXX.kdl")
 
-  cat $"($env.ENVIRONMENTS)/typst/layout.kdl"
+  cat $env.ENVIRONMENTS_TYPST_ZELLIJ_LAYOUT
   | str replace --all "[file]" $file
   | save --force $layout_file
 
